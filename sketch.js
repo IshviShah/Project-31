7651de252108e751f7af1ba4b17fb1f6ae723e97
IshviShah <73232829+IshviShah@users.noreply.github.com>
2021-01-16 11:52:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground;
+
+
+function setup() {
+  createCanvas(480,800);
+  createSprite(400, 200, 50, 50);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(480,height,960,20);
+
+  
+}
+var particles = [];
+var plinkos = [];
+var divisions = [];
+
+var divisionHeight = 300;
+
+function draw() {
+  background(255,255,255);  
+  Engine.update(engine);
+  
+  for(var k = 0; k<=width; k = k+80){
+    divisions.push(new Divisions(k, height-divisionHeight/2,10,divisionHeight));
+  }
+  
+
+  ground.display();
+}
