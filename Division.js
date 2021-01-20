3806b83a739011ffb341c21439fdc4744feaa11c
IshviShah <73232829+IshviShah@users.noreply.github.com>
2021-01-20 09:20:17 +0530

    Update Division.js

--- Division.js
+++ Division.js
@@ -11,7 +11,7 @@ class Division {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("white");
+      fill("black");
       rect(pos.x, pos.y, this.w, this.h);
     }
   };
