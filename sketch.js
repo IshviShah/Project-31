d055a452afb83b44f37d45c0f93645e29c15115d
shirishagajam <64522203+shirishagajam@users.noreply.github.com>
2021-01-21 18:37:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,11 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var ground;
+var particles = [];
+var plinkos = [];
+var divisions = [];
+
+var divisionHeight = 300;
 
 
 function setup() {
@@ -14,22 +19,23 @@ function setup() {
 
   ground = new Ground(480,height,960,20);
 
+  for(var k = 0; k<=width; k = k+80){
+    divisions.push(new Division(k, height-divisionHeight/2,10,divisionHeight));
+  }
+
   
 }
-var particles = [];
-var plinkos = [];
-var divisions = [];
-
-var divisionHeight = 300;
 
 function draw() {
   background(255,255,255);  
   Engine.update(engine);
   
-  for(var k = 0; k<=width; k = k+80){
-    divisions.push(new Division(k, height-divisionHeight/2,10,divisionHeight));
-  }
+ 
   
+  for (var k = 0; k < divisions.length; k++) {
+     
+    divisions[k].display();
+  }
 
   ground.display();
 }
