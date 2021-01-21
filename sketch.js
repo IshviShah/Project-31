80761e80840d620a1b958aa034b0a39efe5e8e7c
IshviShah <73232829+IshviShah@users.noreply.github.com>
2021-01-21 19:46:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,6 +23,12 @@ function setup() {
     divisions.push(new Division(k, height-divisionHeight/2,10,divisionHeight));
   }
 
+  for (var j=40; j<=width; j=j+50){
+    plinkos.push(new Plinko(j,75));
+  }
+  for (var j=15; j<=width-10; j=j+50){
+    plinkos.push(new Plinko(j,175));
+  }
   
 }
 
@@ -30,12 +36,17 @@ function draw() {
   background(255,255,255);  
   Engine.update(engine);
   
- 
-  
   for (var k = 0; k < divisions.length; k++) {
-     
+
     divisions[k].display();
   }
+
+  for (var j = 0; j < plinkos.length; j++) {
+
+    plinkos[j].display();
+  }
+
+  
 
   ground.display();
 }
